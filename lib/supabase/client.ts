85520b10460f0aadd070e989c4d095a35a0b3f7f
SrCosmico <srcosmicoyt@gmail.com>
2026-06-26 09:17:22 -0400

    cambios artur

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient as createSupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://viyodfrzmposupkyeyds.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_ULUTXltcLSlijOHYIaig9w__6N-CHUD';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'key';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'key';
 
 export const createClient = () => {
   return createSupabaseClient(supabaseUrl, supabaseAnonKey);
